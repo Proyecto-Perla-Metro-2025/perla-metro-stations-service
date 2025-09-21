f730858bea8ed25f2a6197e1441f8c9d5bad2763
ignacio Carvajal <ignacio.carvajal@alumnos.ucn.cl>
2025-09-21 12:43:18 -0300

    feat: added applicationDBContext and connection to db with Railway

--- perla-metro-stations-service-api/Program.cs
+++ perla-metro-stations-service-api/Program.cs
@@ -1,6 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+using perla_metro_stations_service_api.src.Data;
 
+Env.Load();
+var builder = WebApplication.CreateBuilder(args);
+var host = Environment.GetEnvironmentVariable("DB_HOST");
+var port = Environment.GetEnvironmentVariable("DB_PORT");
+var user = Environment.GetEnvironmentVariable("DB_USER");
+var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var connectionString = $"Server={host};Port={port};Database={dbName};User={user};Password={password};";
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(connectionString, new MySqlServerVersion(new Version(9, 4, 32))));
+
+var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
